ffbf7f687fdfa17f4555e9bba84d58f3efb8db6e
Vasilis Georgitzikis <tzikis@gmail.com>
2022-07-01 00:33:56 +0300

    Remove Ownable dependency and checking for chainId

--- contracts/TokenBridge.sol
+++ contracts/TokenBridge.sol
@@ -3,16 +3,17 @@ pragma solidity ^0.8.0;
 pragma abicoder v2;
 
 import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
 
-contract TokenBridge is Ownable {
-    event Lock(address token, address owner, uint32 amount, uint32 nonce);
-    event Unlock(address token, address owner, uint32 amount);
+contract TokenBridge{
+    event Lock(uint256 _targetChain, address _token, address _owner, uint32 _amount, uint32 _nonce);
+    event Unlock(uint256 _targetChain, address _token, address _owner, uint32 _amount);
 
 	bytes32 public DOMAIN_SEPARATOR;
     // bytes32 public constant VERIFY_TYPEHASH = keccak256("Verify(string functionName,address tokenAddress,uint256 amount,uint256 nonce)");
-    bytes32 public constant VERIFY_TYPEHASH = keccak256("Verify(string functionName,address tokenAddress,address receiverAddress,uint32 amount,uint32 nonce)");
+    bytes32 public constant VERIFY_TYPEHASH = keccak256("Verify(string functionName,uint256 chainId,address tokenAddress,address receiverAddress,uint32 amount,uint32 nonce)");
+
+    address public owner;
 
     constructor() {
         DOMAIN_SEPARATOR = keccak256(
@@ -25,6 +26,8 @@ contract TokenBridge is Ownable {
                 address(this)
             )
         );
+
+        owner = msg.sender;
     }
 
     function getChainID() public view returns (uint256) {
@@ -38,35 +41,38 @@ contract TokenBridge is Ownable {
     mapping(address => mapping(address => uint32)) public lockedTokens;
     mapping(address => uint32) public lockingNonces;
 
-    function lock(address tokenAddress, uint32 amount) public returns (bool success, bytes memory result) {
+    function lock(uint256 targetChain, address tokenAddress, uint32 amount) public returns (bool success, bytes memory result) {
         require(amount > 0);
+        require(targetChain != getChainID());
+
         (success, result) = tokenAddress.call(abi.encodeWithSelector(ERC20.transferFrom.selector, msg.sender, address(this), amount));
         require(success);
         lockedTokens[tokenAddress][msg.sender] += amount;
 
-        emit Lock(tokenAddress, msg.sender, amount, lockingNonces[msg.sender]++);
+        emit Lock(targetChain, tokenAddress, msg.sender, amount, lockingNonces[msg.sender]++);
     }
 
     mapping(address => mapping(uint32 => bool)) public unlockingNoncesUsed;
 
-    function unlock(address tokenAddress, address receiver, uint32 amount, uint32 nonce, uint8 v, bytes32 r, bytes32 s) public onlyOwner returns (bool success, bytes memory result){
+    function unlock(uint256 targetChain, address tokenAddress, address receiver, uint32 amount, uint32 nonce, uint8 v, bytes32 r, bytes32 s) public returns (bool success, bytes memory result){
         require(amount > 0);
+        require(targetChain == getChainID());
         require(lockedTokens[tokenAddress][receiver] >= amount);
 
         require(unlockingNoncesUsed[receiver][nonce] == false);
         unlockingNoncesUsed[receiver][nonce] = true;
 
-        require(verify("burn()", tokenAddress, receiver, amount, nonce, v, r, s) == owner());
+        require(verify("burn()", targetChain, tokenAddress, receiver, amount, nonce, v, r, s) == owner);
 
         (success, result) = tokenAddress.call(abi.encodeWithSelector(ERC20.transfer.selector, receiver, amount));
         require(success);
         lockedTokens[tokenAddress][receiver] -= amount;
 
-        emit Unlock(tokenAddress, receiver, amount);
+        emit Unlock(targetChain, tokenAddress, receiver, amount);
     }
 
-    event Mint(address _tokenNativeAddress, address _receiver, uint32 _amount, address _wrappedTokenAddress);
-    event Burn(address _tokenNativeAddress, address _receiver, uint32 _amount, address _wrappedTokenAddress, uint32 nonce);
+    event Mint(uint256 _targetChain, address _tokenNativeAddress, address _receiver, uint32 _amount, address _wrappedTokenAddress);
+    event Burn(uint256 _targetChain, address _tokenNativeAddress, address _receiver, uint32 _amount, address _wrappedTokenAddress, uint32 nonce);
 
     struct WrappedTokenParams {
         string name;
@@ -78,39 +84,44 @@ contract TokenBridge is Ownable {
 
     mapping(address => mapping(uint32 => bool)) public mintingNoncesUsed;
 
-    function mint(address tokenNativeAddress, address receiver, uint32 amount, uint32 nonce, WrappedTokenParams memory tokenParams, uint8 v, bytes32 r, bytes32 s) public onlyOwner returns (bool success, bytes memory result){
+    function mint(uint256 targetChain, address tokenNativeAddress, address receiver, uint32 amount, uint32 nonce, WrappedTokenParams memory tokenParams, uint8 v, bytes32 r, bytes32 s) public returns (bytes memory result){
         require(amount > 0);
+        require(targetChain == getChainID());
 
         require(mintingNoncesUsed[receiver][nonce] == false);
         mintingNoncesUsed[receiver][nonce] = true;
 
-        require(verify("lock()", tokenNativeAddress, receiver, amount, nonce, v, r, s) == owner());
+        require(verify("lock()", targetChain, tokenNativeAddress, receiver, amount, nonce, v, r, s) == owner);
 
-        if(wrappedTokenAddresses[tokenNativeAddress] == address(0)){
+        address wrappedTokenAddress = wrappedTokenAddresses[tokenNativeAddress];
+        if(wrappedTokenAddress == address(0)){
             ERC20PresetMinterPauser newERC20Token = new ERC20PresetMinterPauser(string(abi.encodePacked("Wrapped ",tokenParams.name)),string(abi.encodePacked("W", tokenParams.symbol)));
-            wrappedTokenAddresses[tokenNativeAddress] = address(newERC20Token);
+            wrappedTokenAddress = address(newERC20Token);
+            wrappedTokenAddresses[tokenNativeAddress] = wrappedTokenAddress;
             wrappedToNativeTokenAddresses[address(newERC20Token)] = tokenNativeAddress;
         }
+        bool success;
         (success, result) = wrappedTokenAddresses[tokenNativeAddress].call(abi.encodeWithSelector(ERC20PresetMinterPauser.mint.selector, receiver, amount));
         require(success);
-        emit Mint(tokenNativeAddress, receiver, amount, wrappedTokenAddresses[tokenNativeAddress]);
+        emit Mint(targetChain, tokenNativeAddress, receiver, amount, wrappedTokenAddress);
     }
 
     mapping(address => uint32) public burningNonces;
 
     //decide if we should use the native or the non-native address here as a parameter
-    function burn(address tokenWrappedAddress, uint32 amount) public returns (bool success, bytes memory result) {
+    function burn(uint256 targetChain, address tokenWrappedAddress, uint32 amount) public returns (bool success, bytes memory result) {
         require(amount > 0);
+        require(targetChain != getChainID());
 
         address tokenNativeAddress = wrappedToNativeTokenAddresses[tokenWrappedAddress];
 
         (success, result) = tokenWrappedAddress.call(abi.encodeWithSelector(ERC20Burnable.burnFrom.selector, msg.sender, amount));
         require(success);
 
-        emit Burn(tokenNativeAddress, msg.sender, amount, tokenWrappedAddress, burningNonces[msg.sender]++);
+        emit Burn(targetChain, tokenNativeAddress, msg.sender, amount, tokenWrappedAddress, burningNonces[msg.sender]++);
     }
 
-    function verify(string memory functionName, address tokenAddress, address receiverAddress, uint32 amount, uint32 nonce, uint8 v, bytes32 r, bytes32 s) view public returns(address){
+    function verify(string memory functionName, uint256 chainId, address tokenAddress, address receiverAddress, uint32 amount, uint32 nonce, uint8 v, bytes32 r, bytes32 s) view public returns(address){
 
         bytes32 digest =
             keccak256(
@@ -121,6 +132,7 @@ contract TokenBridge is Ownable {
                         abi.encode(
                             VERIFY_TYPEHASH,
                             keccak256(bytes(functionName)),
+                            chainId,
                             tokenAddress,
                             receiverAddress,
                             amount,
