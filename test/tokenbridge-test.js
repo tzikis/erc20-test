9a9cb083ef8701ba9afb45d0913813330793a960
Vasilis Georgitzikis <tzikis@gmail.com>
2022-07-02 22:25:44 +0300

    lock() tests done with full coverage

--- test/tokenbridge-test.js
+++ test/tokenbridge-test.js
@@ -39,6 +39,27 @@ describe("Token Bridge", function () {
     validTargetChainId = chainId + 1;
   });
 
+  it("Should fail when trying to lock 0 tokens", async function () {
+    expect(tokenBridgeContract.lock(chainId+1, sampleTokenAddress, 0)).to.be.revertedWith("");
+  });
+
+  it("Should fail when trying to lock if the user hasn't approved the contract to transferFrom them their tokens", async function () {
+    const targetChain = chainId+1;
+    expect(tokenBridgeContract.lock(targetChain, sampleTokenAddress, 5)).to.be.revertedWith("");
+  });
+
+  it("Should fail when trying to lock if the user hasn't approved the contract to transferFrom them their tokens", async function () {
+    const targetChain = chainId+1;
+    let transactionObject;
+    const allowanceAmount = 100;
+    transactionObject = await tokenSampleContract.approve(tokenBridgeContractAddress, allowanceAmount);
+    await transactionObject.wait();
+    const allowance = await tokenSampleContract.allowance(owner.address, tokenBridgeContractAddress);
+    expect(allowance).to.equal(allowanceAmount);
+
+    expect(tokenBridgeContract.lock(chainId, sampleTokenAddress, allowance)).to.be.revertedWith("");
+  });
+
   it("Should lock a user's native tokens if they have approved the contract to transferFrom them their tokens", async function () {
     let transactionObject;
     const allowanceAmount = 100;
